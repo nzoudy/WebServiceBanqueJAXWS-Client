350fadfa85c64f8c0032dc86aa4d2c3aff2c120d
nzouda urbain <nzoudy@hotmail.fr>
2016-10-27 01:28:09 +0200

    fix bug about the stub

--- WebServiceBanqueJAXWS-Client/src/client/ClientJAXWS.java
+++ WebServiceBanqueJAXWS-Client/src/client/ClientJAXWS.java
@@ -2,23 +2,23 @@ package client;
 
 import java.rmi.RemoteException;
 
+import webservice.Account;
 import webservice.BanqueService;
 import webservice.BanqueServiceProxy;
-import webservice.Compte;
 
 public class ClientJAXWS {
 
 	public static void main(String[] args) {
 
-		BanqueService stub = new BanqueServiceProxy().getBanqueService();
+BanqueService stub = new BanqueServiceProxy().getBanqueService();
 		
 		try {
 			System.out.println("\n 100Euros = "+stub.conversionEuroFcfa(100)+" FCFA");
 			
-			Compte cp = stub.getCompte(2);
+			Account cp = stub.getCompte(2);
 			System.out.println("\n"+cp.getSolde());
 			
-			for (Compte c : stub.listeAllComptes()) {
+			for (Account c : stub.listeAllComptes()) {
 				System.out.println("\n code : " +c.getCode()+" --> Solde = "+c.getSolde());
 			}
 		} catch (RemoteException e) {
